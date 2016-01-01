673247bf26da69b62f8e12d5944b746d63284575
Hiroki Akiyama <akiyama@akiroom.com>
2016-01-02 01:38:51 +0900

    devise detail

--- rails-akiroom-template.rb
+++ rails-akiroom-template.rb
@@ -1,4 +1,6 @@
 
+includesUserLogin = yes?("Include user login?")
+
 # MongoDB
 gem 'bson_ext'
 gem 'mongoid', '~> 5.0', '>= 5.0.1'
@@ -8,12 +10,14 @@ end
 
 # devise
 
-gem 'devise'
-after_bundle do
-  generate 'devise:install'
-  environment "config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }", env: 'development'
-  generate 'devise User'
-  generate 'devise:views'
+if includesUserLogin
+  gem 'devise'
+  after_bundle do
+    generate 'devise:install'
+    environment "config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }", env: 'development'
+    generate 'devise User'
+    generate 'devise:views'
+  end
 end
 
 # Slim
@@ -36,5 +40,18 @@ after_bundle do
   route "root to: 'home#index'"
 end
 
+# Edit some files
+after_bundle do
+  if includesUserLogin
+    inside 'app/views/layouts/' do
+      old_text = '  body\\n'
+      new_text = 'body\\n    p.notice= notice\\n    p.alert= alert\\n'
+      run "ruby -i -pe '$_.gsub!(\"#{old_text}\", \"#{new_text}\")' ./application.html.slim"
+    end
+  end
+end
+
 # git
-run 'git init'
+after_bundle do
+  git :init
+end
